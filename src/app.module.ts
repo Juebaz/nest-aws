caf81859780a1e6143f79260b5ca8fdd4bbbf406
Julie Bazerghi <jbazerghi@nexapp.ca>
2022-03-16 13:27:29 -0400

    added endpoints

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { PostsModule } from './posts/posts.module';
+import { UsersModule } from './users/users.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [],
+  imports: [PostsModule, UsersModule, AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })
